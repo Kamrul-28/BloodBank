80977a36f640b54ac4307715fe17be05582cd961
Md.Kamrul Hasan <kamrul1728@cseku.ac.bd>
2021-08-29 21:18:02 +0600

    pertial components added

---
+++ resources/views/site/pertials/about.blade.php
@@ -0,0 +1,112 @@
+<div id="homepage" class="clear">
+     <div class="tab-wrapper clear">
+       <ul class="tab-nav clear">
+         <li><a href="#tab-1">About Us</a></li>
+         <li><a href="#tab-2">Services</a></li>
+         <li><a href="#tab-3">Our Team</a></li>
+         <li><a href="#tab-4">Latest Projects</a></li>
+       </ul>
+       <div class="tab-container">
+         <!-- Tab Content -->
+         <div id="tab-1" class="tab-content clear">
+           <div class="three_fifth first">
+             <h1 class="emphasise">About Our Company</h1>
+             <p>This is a W3C standards compliant free responsive HTML5 website template from <a href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a>. For full terms of use of this template please read our <a href="https://www.os-templates.com/template-terms">Website Template Licence</a>.</p>
+             <p>You can use and modify the template for both personal and commercial use. You must keep all copyright information and credit links in the template and associated files. For more responsive HTML5 templates visit <a href="https://www.os-templates.com/">Free Website Templates</a>.</p>
+             <p>Quisque lobortis lorem quis metus gravida in egestas elit dapibus. Curabitur nunc mi, mollis in gravida et, malesuada a arcu. Morbi rhoncus adipiscing risus, et tristique velit tempus vitae.</p>
+             <a href="#" class="button large gradient orange">Read More &raquo;</a></div>
+           <div class="two_fifth"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></div>
+         </div>
+         <!-- ## TAB 2 ## -->
+         <div id="tab-2" class="tab-content clear">
+           <ul class="nospace clear">
+             <li class="one_quarter first">
+               <article>
+                 <h1>Headline Text</h1>
+                 <p>Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
+                 <footer><a href="#">Read More &raquo;</a></footer>
+               </article>
+             </li>
+             <li class="one_quarter">
+               <article>
+                 <h1>Headline Text</h1>
+                 <p>Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
+                 <footer><a href="#">Read More &raquo;</a></footer>
+               </article>
+             </li>
+             <li class="one_quarter">
+               <article>
+                 <h1>Headline Text</h1>
+                 <p>Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
+                 <footer><a href="#">Read More &raquo;</a></footer>
+               </article>
+             </li>
+             <li class="one_quarter">
+               <article>
+                 <h1>Headline Text</h1>
+                 <p>Aliquatjusto quisque nam consequat doloreet vest orna partur scetur portortis nam. Metadipiscing eget facilis elit sagittis felisi eger id justo maurisus convallicitur.</p>
+                 <footer><a href="#">Read More &raquo;</a></footer>
+               </article>
+             </li>
+           </ul>
+         </div>
+         <!-- ## TAB 3 ## -->
+         <div id="tab-3" class="tab-content clear">
+           <h2>Team Members</h2>
+           <ul class="nospace clear">
+             <li class="one_quarter first">
+               <figure class="team-member"><img src="images/demo/team-member.gif" alt="">
+                 <figcaption>
+                   <p class="team-name">Name Goes Here</p>
+                   <p class="team-title">Job Title Here</p>
+                   <p class="team-description">Vestassapede et donec ut est liberos sus et eget sed eget quisqueta&hellip;</p>
+                   <p class="read-more"><a href="#">Read More &raquo;</a></p>
+                 </figcaption>
+               </figure>
+             </li>
+             <li class="one_quarter">
+               <figure class="team-member"><img src="images/demo/team-member.gif" alt="">
+                 <figcaption>
+                   <p class="team-name">Name Goes Here</p>
+                   <p class="team-title">Job Title Here</p>
+                   <p class="team-description">Vestassapede et donec ut est liberos sus et eget sed eget quisqueta&hellip;</p>
+                   <p class="read-more"><a href="#">Read More &raquo;</a></p>
+                 </figcaption>
+               </figure>
+             </li>
+             <li class="one_quarter">
+               <figure class="team-member"><img src="images/demo/team-member.gif" alt="">
+                 <figcaption>
+                   <p class="team-name">Name Goes Here</p>
+                   <p class="team-title">Job Title Here</p>
+                   <p class="team-description">Vestassapede et donec ut est liberos sus et eget sed eget quisqueta&hellip;</p>
+                   <p class="read-more"><a href="#">Read More &raquo;</a></p>
+                 </figcaption>
+               </figure>
+             </li>
+             <li class="one_quarter">
+               <figure class="team-member"><img src="images/demo/team-member.gif" alt="">
+                 <figcaption>
+                   <p class="team-name">Name Goes Here</p>
+                   <p class="team-title">Job Title Here</p>
+                   <p class="team-description">Vestassapede et donec ut est liberos sus et eget sed eget quisqueta&hellip;</p>
+                   <p class="read-more"><a href="#">Read More &raquo;</a></p>
+                 </figcaption>
+               </figure>
+             </li>
+           </ul>
+         </div>
+         <!-- ## TAB 4 ## -->
+         <div id="tab-4" class="tab-content clear">
+           <h2>Latest Projects</h2>
+           <ul class="nospace clear">
+             <li class="one_quarter first"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+             <li class="one_quarter"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+             <li class="one_quarter"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+             <li class="one_quarter"><a href="#"><img src="images/demo/gallery.gif" alt=""></a></li>
+           </ul>
+         </div>
+         <!-- / Tab Content -->
+       </div>
+     </div>
+   </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,14 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [DashboardController::class, 'index']);
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
