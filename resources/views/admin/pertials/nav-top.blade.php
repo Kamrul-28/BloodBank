84ad1a16ef75a64e32d7e8da8225bc388cbdc8ab
Md.Kamrul Hasan <kamrul1728@cseku.ac.bd>
2021-10-06 00:30:43 +0600

    Ui modified

--- resources/views/admin/pertials/nav-top.blade.php
+++ resources/views/admin/pertials/nav-top.blade.php
@@ -8,10 +8,10 @@
         
         <!-- Navbar Search-->
         <form action="{{route('blood-search')}}" method="post" class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-            <div class="input-group">
+            {{-- <div class="input-group">
                 <input class="form-control" type="text" placeholder="Search for Blood" aria-label="Search for blood" aria-describedby="btnNavbarSearch" />
                 <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
-            </div>
+            </div> --}}
         </form>
         <!-- Navbar-->
     
